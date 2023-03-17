f2cc056b8457b5aa2023584ec53b3ba0f89aa09c
Olanipekun Adekunle <adekunle.olanipekun.ko@gmail.com>
2023-03-17 02:36:09 +0100

    version 1

---
+++ app.py
@@ -0,0 +1,29 @@
+from joblib import load
+from flask import Flask, request, app, jsonify, url_for,render_template
+import numpy as np
+import pandas as pd
+
+app= Flask(__name__)
+
+# Load the model
+model= load('detectorModel.joblib')
+#loading my scaler
+scaler= load('scaler.joblib')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=["POST"])
+def predict_api():
+    data=request.json['data']
+    n_data= np.array(list(data.values())).reshape(1,-1)
+    n_data= scaler.transform(n_data)
+    prediction= model.predict(n_data)
+    awnser= "no-fraud"
+    if prediction[0]==1:
+        awnser= "A fraudulet act occurs"
+    return jsonify(awnser)
+
+if __name__=="__main__":
+    app.run(debug=True)
